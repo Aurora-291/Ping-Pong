7cff1f50b438b6dfc156eade888a275b5a1a3fd3
Aurora-291 <aurora1425a@gmail.com>
2025-06-11 08:24:40 +0530

    Initial HTML structure and basic setup

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('one-player').addEventListener('click', () => {
+    console.log('One player mode selected');
+});
+
+document.getElementById('two-player').addEventListener('click', () => {
+    console.log('Two player mode selected');
+});
